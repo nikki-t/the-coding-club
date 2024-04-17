2079387b618bd9fbd31030470afda6011893c1d6
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2024-04-17 21:42:10 +0000

    Update Lambda 2 Points to write paths to point files on S3 bucket to JSON file

--- src/sst-lambda-v2/lambda_two_writepoints.py
+++ src/sst-lambda-v2/lambda_two_writepoints.py
@@ -1,10 +1,30 @@
 """
 """
+import json
 import os
-from io import StringIO
+import pathlib
 
 import pandas as pd
 import boto3
+
+
+TMP = pathlib.Path("/tmp")
+
+
+def upload_json(json_data, collection, output_s3_bucket):
+    """Upload JSON data to S3 bucket which will serve as input to Lamba 2."""
+    
+    points_json = TMP.joinpath("points.json")
+    with open(points_json, 'w') as jf:
+        json.dump(json_data, jf, indent=2)
+    
+    session = boto3.Session(profile_name="saml-pub")
+    s3 = session.resource("s3")
+    
+    s3.Bucket(output_s3_bucket).upload_file(points_json, f"{collection}/json/{points_json.name}")
+    print(f"Uploaded: s3://{output_s3_bucket}/{collection}/json/{points_json.name}")
+    
+    points_json.unlink()
 
 
 def lambda_handler_write(event, context):
@@ -28,6 +48,7 @@ def lambda_handler_write(event, context):
     # Write parquet geographic points
     # ------------------------------------------------
 
+    points_json = []
     for i, row in rows_df.iterrows():
 
         pid = row['point_id']
@@ -36,15 +57,12 @@ def lambda_handler_write(event, context):
         output_key = collection_name + '/geo_points/' + str(pid) + '/' + str(ptime) + '.parquet'
 
         # create the temp path for Lambda to write results to locally
-        tmp_file_path = '/tmp/' + output_key
-
-        if not os.path.exists('/tmp/' + collection_name + '/geo_points/' + str(pid) + '/'):
-            print('creating directory: ' + '/tmp/' + collection_name + '/geo_points/' + str(pid) + '/')
-            os.makedirs('/tmp/' + collection_name + '/geo_points/' + str(pid) + '/')
+        tmp_file_path = TMP.joinpath(output_key)
+        tmp_file_path.parent.mkdir(parents=True, exist_ok=True)
 
         # write the results to a parquet file
         try:
-            print('writing file to tmp: ' + tmp_file_path)
+            print('writing file to tmp: ' + str(tmp_file_path))
             out_row = row.to_frame().T
             out_row.to_parquet(tmp_file_path)
         except Exception as e:
@@ -52,5 +70,17 @@ def lambda_handler_write(event, context):
 
         s3_out.upload_file(tmp_file_path, output_s3_bucket, output_key)
 
-        if os.path.exists(tmp_file_path):
-            os.remove(tmp_file_path)
+        tmp_file_path.unlink(missing_ok=True)
+
+        # save results to list
+        points_json.append({
+            "input_s3path": f"s3://{output_s3_bucket}/{output_key}",
+            "collection_name": collection_name,
+            "output_s3bucket": output_s3_bucket
+        })
+        
+    # ------------------------------------------------
+    # Upload list of points JSON to S3 bucket
+    # ------------------------------------------------
+    
+    upload_json(points_json, collection_name, output_s3_bucket)
